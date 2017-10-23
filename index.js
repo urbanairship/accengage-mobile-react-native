5996766db400150ca04003fa6e47d0fc1aaa0986
Bastien Brunaud <bastien@iMac-de-Bastien.local>
2017-10-23 15:25:11 +0200

    fix miscellaneous errors in js

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 'use strict';
 
-import { NativeModules } from 'react-native';
+module.exports = require('./lib/acc');
 
-const { RNAcc } = NativeModules;
 
-//module.exports = require('./lib/acc');
-
-// var Acc = require('./lib/acc');
-// console.log(Acc.inapp.getMsg());
-// console.log(Acc.push.getMsg());
-
-export default RNAcc;
