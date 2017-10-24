4f09b1f17abadc64a0ddd248384fc8446cdc413b
Andrei Mandychev <amandychev@accengage.com>
2017-10-24 19:12:55 +0200

    Android trackEvent is implemented

--- android/src/main/java/com/reactlibrary/analytics/RNAccTrackingModule.java
+++ android/src/main/java/com/reactlibrary/analytics/RNAccTrackingModule.java
@@ -1,12 +1,25 @@
 
 package com.reactlibrary.analytics;
 
+import android.util.Log;
+
+import com.ad4screen.sdk.A4S;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.ReadableNativeMap;
 import com.facebook.react.bridge.Callback;
+import com.reactlibrary.common.Utils;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RNAccTrackingModule extends ReactContextBaseJavaModule {
+
+    private static final String TAG = "AccTracking";
 
     private final ReactApplicationContext reactContext;
 
@@ -21,8 +34,28 @@ public class RNAccTrackingModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void getMsg() {
-        android.util.Log.d("AccTrackinModule", "getMsg from AccTracking!!!!");
+    public void trackEvent(int key) {
+        Log.d(TAG, "trackEvent key: " + key);
+        A4S.get(getReactApplicationContext()).trackEvent(key);
+    }
+
+    @ReactMethod
+    public void trackEvent(int key, ReadableMap customData) {
+        if (customData == null) {
+            this.trackEvent(key);
+            return;
+        }
+        if (!(customData instanceof ReadableNativeMap)) {
+            Log.w(TAG, "Custom data is sent in unsuported type and ignored");
+            this.trackEvent(key);
+            return;
+        }
+
+        Map hashMap = Utils.recursivelyDeconstructReadableMap(customData);
+        JSONObject jsonObject = new JSONObject(hashMap);
+
+        Log.d(TAG, "trackEvent key: " + key + ", obj: " + jsonObject.toString());
+        A4S.get(getReactApplicationContext()).trackEvent(key, jsonObject.toString());
     }
 
 }
---
+++ android/src/main/java/com/reactlibrary/common/Utils.java
@@ -0,0 +1,77 @@
+package com.reactlibrary.common;
+
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.ReadableMapKeySetIterator;
+import com.facebook.react.bridge.ReadableType;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Utils {
+
+    public static Map<String, Object> recursivelyDeconstructReadableMap(ReadableMap readableMap) {
+        ReadableMapKeySetIterator iterator = readableMap.keySetIterator();
+        Map<String, Object> deconstructedMap = new HashMap<>();
+        while (iterator.hasNextKey()) {
+            String key = iterator.nextKey();
+            ReadableType type = readableMap.getType(key);
+            switch (type) {
+                case Null:
+                    deconstructedMap.put(key, null);
+                    break;
+                case Boolean:
+                    deconstructedMap.put(key, readableMap.getBoolean(key));
+                    break;
+                case Number:
+                    deconstructedMap.put(key, readableMap.getDouble(key));
+                    break;
+                case String:
+                    deconstructedMap.put(key, readableMap.getString(key));
+                    break;
+                case Map:
+                    deconstructedMap.put(key, recursivelyDeconstructReadableMap(readableMap.getMap(key)));
+                    break;
+                case Array:
+                    deconstructedMap.put(key, recursivelyDeconstructReadableArray(readableMap.getArray(key)));
+                    break;
+                default:
+                    throw new IllegalArgumentException("Could not convert object with key: " + key + ".");
+            }
+
+        }
+        return deconstructedMap;
+    }
+
+    public static List<Object> recursivelyDeconstructReadableArray(ReadableArray readableArray) {
+        List<Object> deconstructedList = new ArrayList<>(readableArray.size());
+        for (int i = 0; i < readableArray.size(); i++) {
+            ReadableType indexType = readableArray.getType(i);
+            switch(indexType) {
+                case Null:
+                    deconstructedList.add(i, null);
+                    break;
+                case Boolean:
+                    deconstructedList.add(i, readableArray.getBoolean(i));
+                    break;
+                case Number:
+                    deconstructedList.add(i, readableArray.getDouble(i));
+                    break;
+                case String:
+                    deconstructedList.add(i, readableArray.getString(i));
+                    break;
+                case Map:
+                    deconstructedList.add(i, recursivelyDeconstructReadableMap(readableArray.getMap(i)));
+                    break;
+                case Array:
+                    deconstructedList.add(i, recursivelyDeconstructReadableArray(readableArray.getArray(i)));
+                    break;
+                default:
+                    throw new IllegalArgumentException("Could not convert object at index " + i + ".");
+            }
+        }
+        return deconstructedList;
+    }
+}
