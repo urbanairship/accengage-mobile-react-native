fa92a3b79f494bbd740bb0e4938d0d3abc911160
Antoine Pery-Kasza <antoine.pery-kasza@urbanairship.com>
2019-10-16 17:00:58 +0200

    Autolinking now working, one package. But Appregistry error

--- example/index.js
+++ example/index.js
@@ -1,5 +1,9 @@
+/**
+ * @format
+ */
+
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from "./app.json";  
-//require('RCTNativeAppEventEmitter');
+
 AppRegistry.registerComponent(appName, () => App);
