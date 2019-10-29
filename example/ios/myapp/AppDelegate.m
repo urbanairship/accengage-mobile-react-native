d2dd270fdd6e06fb3eaf605d8748e88ca73da1b6
Bastien MATTHÄI <bastien_matthai1@hotmail.fr>
2019-10-29 15:48:44 +0100

    modify files for 0.61.2

--- example/ios/myapp/AppDelegate.m
+++ example/ios/myapp/AppDelegate.m
@@ -9,54 +9,61 @@
 
 #import "AppDelegate.h"
 
+#import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 #import <React/RCTLinkingManager.h>
-#import <Accengage/Accengage.h>
+//#import <Accengage/Accengage.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  ACCConfiguration *config = [ACCConfiguration defaultConfig];
-  config.launchOptions = launchOptions;
-  
-  BOOL dataOptin = [[[NSUserDefaults standardUserDefaults] valueForKey:@"dataOptinApp"] boolValue];
-  BOOL geolocOptin = [[[NSUserDefaults standardUserDefaults] valueForKey:@"geolocOptinApp"] boolValue];
-  
-  dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(),^{
-    [Accengage startWithConfig:config optIn:ACCOptInEnabled];
-    if (dataOptin) {
-      [Accengage setDataOptInEnabled:dataOptin];
-    }
-    if (geolocOptin) {
-      [Accengage setGeolocOptInEnabled:geolocOptin];
-    }
-    [Accengage setLoggingEnabled:YES];
-  });
+//    ACCConfiguration *config = [ACCConfiguration defaultConfig];
+//    config.launchOptions = launchOptions;
+//    
+//    BOOL dataOptin = [[[NSUserDefaults standardUserDefaults] valueForKey:@"dataOptinApp"] boolValue];
+//    BOOL geolocOptin = [[[NSUserDefaults standardUserDefaults] valueForKey:@"geolocOptinApp"] boolValue];
+//    
+//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(),^{
+//        [Accengage startWithConfig:config optIn:ACCOptInEnabled];
+//        if (dataOptin) {
+//            [Accengage setDataOptInEnabled:dataOptin];
+//        }
+//        if (geolocOptin) {
+//            [Accengage setGeolocOptInEnabled:geolocOptin];
+//        }
+//        [Accengage setLoggingEnabled:YES];
+//    });
     
-  NSURL *jsCodeLocation;
+    RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
+    
+    RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
+                                                     moduleName:@"myapp"
+                                              initialProperties:nil];
+    rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+    
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    UIViewController *rootViewController = [UIViewController new];
+    rootViewController.view = rootView;
+    self.window.rootViewController = rootViewController;
+    [self.window makeKeyAndVisible];
+    return YES;
+}
 
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
-
-  RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
-                                                      moduleName:@"myapp"
-                                               initialProperties:nil
-                                                   launchOptions:launchOptions];
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-
-  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-  UIViewController *rootViewController = [UIViewController new];
-  rootViewController.view = rootView;
-  self.window.rootViewController = rootViewController;
-  [self.window makeKeyAndVisible];
-  return YES;
+- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
+{
+#if DEBUG
+    return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+#else
+    return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+#endif
 }
 
 - (BOOL)application:(UIApplication *)application
             openURL:(NSURL *)url
             options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
-  return [RCTLinkingManager application:application openURL:url options:options];
+    return [RCTLinkingManager application:application openURL:url options:options];
 }
 
 @end
